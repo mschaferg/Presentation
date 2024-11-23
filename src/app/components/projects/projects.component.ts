7e1402152e4a84c0766545ef201568771d271895
Schafer Gray <schafergray@Schafers-MacBook-Air.local>
2024-11-23 11:16:43 -0500

    Updating assets.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -20,13 +20,13 @@ export class ProjectsComponent {
       name: "Expense Tracker",
       description: "Like Mint, but worse.",
       link: "https://mschaferg.github.io/angular-expense-tracker",
-      imgSrc: "../../../assets/Expense-App.png"
+      imgSrc: "assets/Expense-App.png"
     },
     {
       name: "Point of Sale App",
       description: "I had to make this when I started my job. It's a pos POS. (It's not actually hosted online so this will just take you to a small website called Google).",
       link: "https://google.com",
-      imgSrc: "../../../assets/Point-Of-Sale-App.png"
+      imgSrc: "assets/Point-Of-Sale-App.png"
     }
   ];
 
